b0c5739c2837a553e3fc76b50ff02d041a1b2623
Anmol Verma <anmolverma102002@gmail.com>
2022-01-24 17:04:01 +0530

    Added port and js stuff

--- client/index.js
+++ client/index.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
+  fetch("http://localhost:5000/getAll")
+    .then((responce) => responce.json())
+    .then((data) => console.log(data));
   loadHTMLTable([]);
 });
 
 function loadHTMLTable(data) {
   const table = document.querySelector("table tbody");
-//   const tableHtml = "";
+  //   const tableHtml = "";
   if (data.length === 0) {
-    table.innerHTML = "<tr><td class='no-data' colspan = '5' >No Data</td></tr>";
+    table.innerHTML =
+      "<tr><td class='no-data' colspan = '5' >No Data</td></tr>";
   }
 }
