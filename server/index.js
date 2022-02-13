8d7cf2c6e9d5caf55ca40cba4013488cfc9108a6
Anmol Verma <anmolverma102002@gmail.com>
2022-02-13 11:15:04 +0530

    Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
-import express from "express";//We can use import instead of require just add - "type":"module" in the package.json
+import express from "express"; //We can use import instead of require just add - "type":"module" in the package.json
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-const app = express;
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true })); //Image size upload limit - 30 MB
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //Image size upload limit - 30 MB
+app.use(cors);
+
+const CONNECTION_URL =
+  "mongodb+srv://anmolDB:<password>@cluster0.qsmtv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
