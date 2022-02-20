9122c4518858443a4bc4ae24980920a895da7f6b
Anmol Verma <anmolverma102002@gmail.com>
2022-02-20 11:45:03 +0530

    Not updating on submit bug

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,4 +5,4 @@ const url = "http://localhost:5000/posts"; //This will collect all the data from
 export const fetchPosts = () => axios.get(url);
 //Below function will get full post inputed by user from form
 export const createPost = (newPost) => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`,updatedPost);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
