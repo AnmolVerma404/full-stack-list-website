33ced25da55264b56e94d473492fca7b2c54a691
Anmol Verma <anmolverma102002@gmail.com>
2022-02-20 22:21:05 +0530

    �Setting ENV

--- server/index.js
+++ server/index.js
@@ -2,23 +2,22 @@ import express from "express"; //We can use import instead of require just add -
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv';
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true })); //Image size upload limit - 30 MB
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //Image size upload limit - 30 MB
 app.use(cors());//But at c2ba31c was here, forgot to add () after cores and some typo mistake in controllers/posts.js
 
 app.use("/posts", postRoutes); //Define this after app.use(cors); to not get network error
-const CONNECTION_URL =
-  "mongodb+srv://anmolDB:anmolDB1710@cluster0.qsmtv.mongodb.net/memo?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
