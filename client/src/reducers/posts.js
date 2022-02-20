620b543944265af237fbd9e31968f91fa633f351
Anmol Verma <anmolverma102002@gmail.com>
2022-02-20 22:10:52 +0530

    Adding constant variables

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,16 +1,16 @@
+import { FETCH_ALL,CREATE,UPDATE,DELETE } from "../constants/actionTypes.js";
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post._id !== action.payload);
-    case "UPDATE":
-    case "LIKE"://As LIKE and UPDATE have same return so we can write them one after other
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
     default:
       return posts;
